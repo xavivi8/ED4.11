6c0bcade3d49b503f9d7848b8d12fae99149cae6
DAM1 <DAM1@DESKTOP-3LHBG1B>
2023-05-10 19:10:57 +0200

    El paso 3

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Estamos haciendo control de versiones con Git");
 		System.out.println("Otro cambio");
+		System.out.println("El paso 3");
 	}
 
 }

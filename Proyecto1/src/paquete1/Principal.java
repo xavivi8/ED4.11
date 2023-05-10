755a37224d92082b26a2d260fbedd267ac96d3cb
DAM1 <DAM1@DESKTOP-3LHBG1B>
2023-05-10 19:10:20 +0200

    Paso 2

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Estamos haciendo control de versiones con Git");
+		System.out.println("Otro cambio");
 	}
 
 }
